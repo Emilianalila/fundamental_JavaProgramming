e74ad7a150c57103406f5773a7eb88b49cd82bc3
Emilianalila <emilianalila@gmail.com>
2024-05-05 15:33:03 +0200

    refactor exceptions

--- src/main/java/org/example/chapter13_Exceptions/ExceptionsHandling.java
+++ src/main/java/org/example/chapter13_Exceptions/ExceptionsHandling.java
@@ -25,6 +25,9 @@ public class ExceptionsHandling {
             // Code to handle the exception
             System.out.println("Directory does not exist.");
             e.printStackTrace();
+           /* e.printStackTrace();
+            e.printStackTrace();*/
+            
         }
     }
 
